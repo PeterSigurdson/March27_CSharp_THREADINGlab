e086a4940d4126dbbcf35483275c3d1ad2aed49b
Peter Sigurdson <Peter.Sigurdson@Tor.Loc>
2019-03-27 10:18:26 -0400

    initial commit

---
+++ ThreadingWorkshop/ThreadingWorkshop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadingWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Download();
+
+        }
+
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Complete");
+        }
+    }
+}
