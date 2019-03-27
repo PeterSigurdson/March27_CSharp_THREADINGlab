7ab2d9494743338739f4436088add8ef3d1b72b2
Peter Sigurdson <Peter.Sigurdson@Tor.Loc>
2019-03-27 11:56:54 -0400

    11:56 commit

--- ThreadingWorkshop/ThreadingWorkshop/Program.cs
+++ ThreadingWorkshop/ThreadingWorkshop/Program.cs
@@ -2,37 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace LearningAsyncAwayStart
+namespace LearningProperties
 {
+    class Player
+    {
+        public bool isAlive = true;
+        int health = 100;
+
+        public void Hit()
+        {
+            Random r = new Random();
+            health -= r.Next(5, 50);
+        }
+
+        public void CheckDeath()
+        {
+            if (health <= 0)
+                isAlive = false;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("downloading file");
-            Download();
-            Console.ReadLine();
-
-        }
-
-        static void Download()
-        {
-            Network.Download((message) => Console.WriteLine("Download complete " + message));
-        }
-    }
-
-    // Imaginary external network library
-
-    class Network
-    {
-        static public void Download(Action<String> callback)
-        {
-            Task.Run(() => {
-                Thread.Sleep(3000);
-                callback("completed");
-            });
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                player.CheckDeath();
+                Console.WriteLine("Is player alive: " + player.isAlive);
+            }
         }
     }
 }
